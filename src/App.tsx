2467807e6e2e1e2672c4dd802e70cea33096e71b
Dev-MoyoS <moyosbi2007@gmail.com>
2025-12-12 21:44:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ import Index from "./pages/Index";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
 
 const queryClient = new QueryClient();
 
