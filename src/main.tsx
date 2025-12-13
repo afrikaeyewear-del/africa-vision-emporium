3f102a201cf3d6e633e1a5ffcc207e8584038960
Dev-MoyoS <moyosbi2007@gmail.com>
2025-12-13 09:56:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { initScrollAnimations } from "./utils/scroll-animations";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-// Initialize scroll animations
-if (typeof window !== "undefined") {
-  // Wait for DOM to be ready
-  setTimeout(() => {
-    initScrollAnimations();
-  }, 100);
-}
